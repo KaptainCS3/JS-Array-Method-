fef588f7c770dbae376de17d85a4cd86656ef0ee
KaptainCS3 <mbiapplegryn@gmail.com>
2022-08-19 21:27:04 +0100

    initial commit

---
+++ Map/app.js
@@ -0,0 +1,8 @@
+// map method iterating over arrays and objects
+// return square values from nums array 
+const nums = [1, 2, 3, 4, 5, 6,7, 8, 9];
+
+const squareValue = nums.map(items =>{
+    return items ** 2;
+})
+console.log(squareValue);
